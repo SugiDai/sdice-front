a1dafc4338ea122742b03a056988b28c5b49daad
杉山大輔 <dsugiyama@sdicesoft.com>
2018-10-17 15:18:02 +0900

    move blog-fron

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,8 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  PROTOCOL:'"https://"',
-  API_SITE_ENDPOINT: '"https://localhost:8000/api/site"',
-  SITE_SITE_DETAIL: '"https://localhost:8000/api/sitedetaile/"',
-  FILE_ENDPOINT: '"https://localhost:8000"',
+  PROTOCOL:'"http://"',
+  API_SITE_ENDPOINT: '"http://localhost:8000/api/site"',
+  SITE_SITE_DETAIL: '"http://localhost:8000/api/sitedetaile/"',
+  FILE_ENDPOINT: '"http://localhost:8000"',
 }
